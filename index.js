f5af435c80c4268ea8a6c59503c9fd784a368dd2
Shawn Koski <shawnnkoski@gmail.com>
2018-03-05 11:45:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var recipes = {};
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  var newRecipes.assign({}, recipes, {[key]:  value});
+  var newRecipes = {};
+  Object.assign(newRecipes, recipes, {[key]:  value});
   return newRecipes;
 }
